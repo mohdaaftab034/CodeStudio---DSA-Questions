b67b20e255e61348bcba8ab6ce5aa227b4b4bdfe
Mohd Aaftab <mohdaaftab8630@gmail.com>
2026-03-07 14:27:26 +0530

    Update Search In A Grid.cpp

--- Matrix/Search In A Grid.cpp
+++ Matrix/Search In A Grid.cpp
@@ -27,3 +27,37 @@ bool searchGrid(vector<vector<int>>& mat, int n, int m, int target)
 
     return false;
 }
+
+
+
+// -----------------Solution------------------
+#include <bits/stdc++.h> 
+bool searchGrid(vector<vector<int>>& mat, int n, int m, int target)
+{
+    for(int i = 0; i < n; i++) {
+        if(mat[i][0] <= target && mat[i][m-1] >= target) {
+
+            // Binary search
+            int start = 0;
+            int end = m-1;
+
+            while(start <= end) {
+                int mid = start + (end - start) / 2;
+
+                if(mat[i][mid] == target) {
+                    return true;
+                }
+
+                else if(mat[i][mid] < target) {
+                    start = mid + 1;
+                }
+
+                else {
+                    end = mid - 1;
+                }
+            }
+        }
+    }
+
+    return false;
+}
