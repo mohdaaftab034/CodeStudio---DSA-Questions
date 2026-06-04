355accd8faf7b1912df160aecd62a9bdb21ed780
Mohd Aaftab <mohdaaftab8630@gmail.com>
2026-06-04 23:37:01 +0530

    Create  Valid Parentheses.cpp

---
+++ Stack/Valid Parentheses.cpp
@@ -0,0 +1,33 @@
+// Problem - https://www.naukri.com/code360/problems/valid-parenthesis_795104?utm=codolio&leftPanelTabValue=PROBLEM
+
+
+
+
+
+// --------------------Solution-------------------------
+bool isValidParenthesis(string s)
+{
+    int n = s.size();
+
+    stack<char> st;
+
+    for(char ch: s) {
+
+        if(ch == '[' || ch == '(' || ch == '{') {
+            st.push(ch);
+        } 
+
+        else {
+            
+            if(st.empty()) {
+                return false;
+            }
+
+            if((ch == '}' && st.top() != '{') || (ch == ']' && st.top() != '[') || (ch == ')' && st.top() != '(')) {
+                return false;
+            }
+            st.pop();
+        }
+    }
+    return st.empty();
+}
